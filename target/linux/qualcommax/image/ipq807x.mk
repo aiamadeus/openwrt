380c7339c370cf23dd1cb80eff4ed625eaab8378
Chukun Pan <amadeus@jmu.edu.cn>
2026-06-06 18:04:18 +0800

    qualcommax: add TP-Link TL-ER2260T support
    Hardware specification:
      SoC: Qualcomm IPQ8070A
      Flash: F59D1G81MB 128MB
      RAM: M15T4G16256A 512MB
      Ethernet1: 4x 1Gbps (QCA8075)
      Ethernet2: 2x 10Gbps SFP+
      USB: 1 x USB 2.0
      Button: Reset
    
    Flash instructions:
      1. Solder 0-ohm resistors at R6317 and R6318.
         (next to the TTL)
    
      2. Download the initramfs image, rename it to
         initramfs.itb, and host it with tftp server.
    
      3. Press Ctrl+B to interrupt U-Boot. Enter
         the following command to set bootcmd:
    
         setenv mtdids 'nand0=nand0'
         setenv mtdparts 'mtdparts=nand0:0x7000000@0x720000(rootfs)'
         setenv boot_ubi 'ubi part rootfs; ubi read 0x44000000 kernel; bootm 0x44000000'
         setenv bootcmd 'run boot_ubi'
         saveenv
    
      4. Booting OpenWrt initramfs:
    
         tftpboot initramfs.itb
         bootm
    
      5. After openwrt boots up, use scp or luci web
         to upload sysupgrade.bin to upgrade.
    
    Signed-off-by: Chukun Pan <amadeus@jmu.edu.cn>

--- target/linux/qualcommax/image/ipq807x.mk
+++ target/linux/qualcommax/image/ipq807x.mk
@@ -509,6 +509,19 @@ define Device/tplink_eap660hd-v1
 endef
 TARGET_DEVICES += tplink_eap660hd-v1
 
+define Device/tplink_tl-er2260t
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := TP-Link
+	DEVICE_MODEL := TL-ER2260T
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	SOC := ipq8070
+	DEVICE_PACKAGES := kmod-i2c-mux-gpio kmod-sfp kmod-usb-ledtrig-usbport \
+		-ath11k-firmware-ipq8074 -kmod-ath11k-ahb -wpad-basic-mbedtls
+endef
+TARGET_DEVICES += tplink_tl-er2260t
+
 define Device/xiaomi_ax3600
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
--- target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
@@ -187,6 +187,7 @@ platform_do_upgrade() {
 	netgear,wax218|\
 	netgear,wax620|\
 	netgear,wax630|\
+	tplink,tl-er2260t|\
 	zyxel,nwa110ax|\
 	zyxel,nwa210ax)
 		nand_do_upgrade "$1"
